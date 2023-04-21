427807bae7a8d44234f719330316701d23d49380
oolcha <olha.bondarenko.job@gmail.com>
2023-04-21 20:44:00 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,6 +28,17 @@
  */
 
 function calculateRentalCost(days) {
+  const fullCost = days * 40;
+
+  if (days >= 7) {
+    return fullCost - 50;
+  }
+
+  if (days >= 3) {
+    return fullCost - 20;
+  }
+
+  return fullCost;
 }
 
 module.exports = calculateRentalCost;
